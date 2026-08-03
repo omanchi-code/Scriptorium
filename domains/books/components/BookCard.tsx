63ee468ba44d3a3e349e3d2dbf0e5046ac9f18c1
Omanchi-Job Agbo <almighty336@gmail.com>
2026-08-03 20:20:03 +0100

    Fix BookCard data import

--- domains/books/components/BookCard.tsx
+++ domains/books/components/BookCard.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Card from "@/components/ui/Card";
-import type { Book } from "./data";
+import type { Book } from "../data";
 
 export default function BookCard({
   book,
